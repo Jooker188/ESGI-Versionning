ec9e77ad7ee0614d6a09454319ab3609645a975d
Vicktor <leezerdparis@gmail.com>
2023-11-30 01:10:21 +0000

    Correctif branche nom

--- code/nom.cpp
+++ code/nom.cpp
@@ -2,8 +2,24 @@ include "string.h"
 
 string choixNom(string nom)
 {
+
     if(strlen(nom) >= 15 || strlen(nom) <= 1 )
-        return nom;
+    {
+        bool isValid=true;
+        int nomLen=strlen(nom);
+
+        for(int i=0; i<nomLen; i++)
+        {
+            if(((nom[i] >= 'A' && nom[i] <= 'Z')) || ((nom[i] >= 'a' && nom[i] <= 'z')))
+                return nom;
+            else
+            {
+                printf("nom invalide");
+                break;
+            }
+        }
+
+    }
     else
         printf("nom invalide");
 }
